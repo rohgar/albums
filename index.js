2334022e4a76c19075ba02ac7c0eb206bf9a693c
garroh <garroh99@gmail.com>
2018-03-21 09:06:04 -0700

    Updated app.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Import a library to help create a Component
-import React from 'react'
-import ReactNative from 'react-native'
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
 // Create the compnonent
+
+// const App = () => {
+//     return (
+//         <Text>Some Text</Text>
+//     );
+// };
+const App = () =>
+(
+    <Text>Some Text</Text>
+);
+
+
 // Render it to the device
+AppRegistry.registerComponent('albums', () => App);
