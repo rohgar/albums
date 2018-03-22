df63431015b440a849d9fcf7c31402be3dab5bfb
garroh <garroh99@gmail.com>
2018-03-21 22:23:40 -0700

    Added a reusable header component

---
+++ src/components/header.js
@@ -0,0 +1,35 @@
+// Import a library to help create a Component
+import React from 'react';
+import { Text , View } from 'react-native';
+
+// Create the compnonent
+
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 70,
+        paddingTop: 15,
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.4,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 22
+    }
+};
+
+// Make to component available
+export default Header;
